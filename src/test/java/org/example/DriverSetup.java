7453c72b50007828684ac020d0b75553852b17a7
ahmad farichin <ahmadfarichin98@gmail.com>
2025-11-26 16:55:57 +0700

    hapus comment pada code

--- src/test/java/org/example/DriverSetup.java
+++ src/test/java/org/example/DriverSetup.java
@@ -1,29 +1,3 @@
-/*package org.example;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-public class DriverSetup {
-    private static WebDriver driver;
-
-    public static WebDriver getDriver() {
-        if (driver == null) {
-            ChromeOptions options = new ChromeOptions();
-            driver = new ChromeDriver(options);
-            driver.manage().window().maximize();
-        }
-        return driver;
-    }
-
-    public static void quitDriver() {
-        if (driver != null) {
-            driver.quit();
-            driver = null;
-        }
-    }
-}*/
-
 package org.example;
 
 import org.openqa.selenium.WebDriver;
